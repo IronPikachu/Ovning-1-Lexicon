acb57ab3b0e82f8e91de361765d26ec44932bb1e
IronPikachu <simon.pikachu@hotmail.se>
2021-11-30 11:57:24 +0100

    Add project files.

---
+++ Övning 1 Lexicon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Övning_1_Lexicon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
